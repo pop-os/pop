87777a9e377a07e9aa9ac27e61c2aeaf1c3833ae
Jeremy Soller <jackpot51@gmail.com>
2026-05-07 11:14:56 -0600

    Fix key for Ubuntu 22.04 and later builds

--- scripts/pop-ci/src/repo.rs
+++ scripts/pop-ci/src/repo.rs
@@ -106,8 +106,7 @@ impl RepoInfo {
                 },
                 // apt.pop-os.org for Ubuntu 22.04 and later
                 _ => Self {
-                    key: fs::canonicalize("scripts/.ppa-dev.asc")
-                        .expect("failed to find dev PPA key"),
+                    key: fs::canonicalize("scripts/.iso.asc").expect("failed to find dev PPA key"),
                     release: "http://apt.pop-os.org/release-ubuntu",
                     staging: "http://apt.pop-os.org/staging-ubuntu/master",
                     dput: Some("ppa:system76-dev/pre-stable"),
